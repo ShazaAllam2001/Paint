edd157bb37255225420e8386c471e4ceee87c24b
ShazaAllam2001 <shazaallam90@gmail.com>
2021-12-04 17:41:04 +0200

    intial fron-end design

--- paint-front/src/app/app.module.ts
+++ paint-front/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { ToolBoxComponent } from './tool-box/tool-box.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    CanvasComponent,
+    ToolBoxComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule,
