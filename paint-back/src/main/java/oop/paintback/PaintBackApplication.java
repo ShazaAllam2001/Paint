54d709c848ca3693b7079def454dbc2e2b12fa70
ShazaAllam2001 <shazaallam90@gmail.com>
2021-12-06 23:14:16 +0200

    intial back

---
+++ paint-back/src/main/java/oop/paintback/PaintBackApplication.java
@@ -0,0 +1,13 @@
+package oop.paintback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBackApplication.class, args);
+	}
+
+}
