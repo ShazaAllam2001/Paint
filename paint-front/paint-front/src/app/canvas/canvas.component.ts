1b699db323d3fbd5b1ec4a046084edfe6759b702
rowannasser <78316754+rowannasser@users.noreply.github.com>
2021-12-07 07:38:22 +0200

    menu bar

---
+++ paint-front/paint-front/src/app/canvas/canvas.component.ts
@@ -0,0 +1,69 @@
+
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.css']
+})
+export class CanvasComponent implements OnInit {
+  currWidth = 1140;
+  currHeight = 490;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.drawGrid();
+  }
+
+  drawGrid() {
+    var cnv: HTMLCanvasElement | null;
+    var ctx;
+    cnv = document.getElementsByTagName("canvas")[0];
+    ctx = cnv?.getContext("2d");
+    /* small scale */
+    // vertical lines
+    for(var i=10; i<this.currWidth; i+=10) {
+      if(ctx) {
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,this.currHeight);
+        ctx.strokeStyle = "#E8E8E8";
+        ctx.stroke();
+      }
+    }
+    // horizontal lines
+    for(var i=10; i<this.currHeight; i+=10) {
+      if(ctx) {
+        ctx.moveTo(0,i);
+        ctx.lineTo(this.currWidth,i);
+        ctx.strokeStyle = "#E8E8E8";
+        ctx.stroke();
+      }
+    }
+
+    ctx?.beginPath();
+    /* large scale */
+    // vertical lines
+    for(var j=50; j<this.currWidth; j+=50) {
+      if(ctx) {
+        ctx.moveTo(j,0);
+        ctx.lineTo(j,this.currHeight);
+        ctx.strokeStyle = "#A0A0A0";
+        ctx.stroke();
+      }
+    }
+    // horizontal lines
+    for(var j=50; j<this.currHeight; j+=50) {
+      if(ctx) {
+        ctx.moveTo(0,j);
+        ctx.lineTo(this.currWidth,j);
+        ctx.strokeStyle = "#A0A0A0";
+        ctx.stroke();
+      }
+    }
+  }
+
+}
